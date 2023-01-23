352fb7a0e3e03bb8b5e631d73c8c48e46f496459
drzoi <yellow_one@mail.ru>
2023-01-23 15:30:23 +0300

    Initial commit

---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,11 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component ("timer")
+public class Timer {
+    private Long nanoTime = System.nanoTime();
+    public Long getTime() {
+        return nanoTime;
+    }
+}
